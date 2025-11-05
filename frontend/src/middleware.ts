08a18f69eee8b577f4253c23d4d730bc764b8089
ChefToan <chefpeter0812@gmail.com>
2025-11-04 22:14:08 -0700

    Added Plausible tracking 2

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -14,7 +14,7 @@ export function middleware() {
   if (process.env.NODE_ENV === 'production') {
     response.headers.set(
       'Content-Security-Policy',
-      "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:;"
+      "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' plausible.io; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https: plausible.io;"
     )
   }
   
